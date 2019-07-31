c0e01b771eafdb655bc3bbdbef067ab18e465538
nanditha16 <nanditha.murthy@gmail.com>
2019-07-31 18:19:00 -0400

    OCP Practice

---
+++ examOCP/src/main/java/com/examOCP/Card.java
@@ -0,0 +1,13 @@
+package com.examOCP;
+
+/**
+ * Hello world!
+ *
+ */
+public class Card 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
